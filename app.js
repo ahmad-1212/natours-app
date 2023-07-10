94b56904a00ab17bbbe9a6004d6c75ecc3fe0445
ahmad1212 <ahmadali.swat333@gmail.com>
2023-07-10 12:58:52 +0500

    Improved stripe implementaion

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 
 // Start express app
 const app = express();
@@ -52,6 +53,17 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
+
+// We use this route in app.js instead of using it in booking route because the reason for that
+// is when we recieve the body from stripe, the stripe function than we are gonna use to actually
+// read the body needs this body in a raw form so basically as a string and not as JSON
+// So again in simple word in this route here, we need the body coming with the request to be not in JSON, otherwise this is not going to working at all
+// And as soon as the request hits the middleware express.json() the body is converted to JSON
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/octet-stream' }),
+  bookingController.webhookCheckout
+);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
